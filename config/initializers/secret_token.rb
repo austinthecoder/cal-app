0567a0a57ec4c38aed1b733dab7647fc4e372783
Austin Schneider <austinthecoder@gmail.com>
2012-06-07 01:16:22 -0400

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalApp::Application.config.secret_token = '826d29d154b2e8ded94aa959ca3e196b2fcfa9df677db1118d3b2bf1263d68fda8a32731d499324715886e9a68bb51721f6af7e559d16269910ec94defa01fe8'
